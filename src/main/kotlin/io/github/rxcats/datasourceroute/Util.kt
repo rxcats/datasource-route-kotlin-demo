80497243831e5f8a7404a1ade7ee042e0dffdad8
rxcats <windpyj80@gmail.com>
2019-06-25 17:24:09 +0900

    logger 수정

--- src/main/kotlin/io/github/rxcats/datasourceroute/Util.kt
+++ src/main/kotlin/io/github/rxcats/datasourceroute/Util.kt
@@ -2,7 +2,14 @@ package io.github.rxcats.datasourceroute
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import kotlin.math.abs
 
-fun getShardNo(userId: String, targets: ArrayList<Int>) = targets[Math.abs(userId.hashCode() % targets.size)]
+fun getShardNo(userId: String, targets: ArrayList<Int>) = targets[abs(userId.hashCode() % targets.size)]
 
-inline fun <reified T> T.logger(): Logger = LoggerFactory.getLogger(T::class.java)
+inline fun <reified T> T.logger(): Logger {
+    return if (T::class.isCompanion) {
+        LoggerFactory.getLogger(T::class.java.enclosingClass)
+    } else {
+        LoggerFactory.getLogger(T::class.java)
+    }
+}
